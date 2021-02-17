ffcd3e350c4ae41bea23099cc908531b6b2e5788
KarolKul-KK <kakulinski@wp.pl>
2021-02-17 17:34:24 +0100

    master

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    # load data from database
+    # connect with database
     from models import User, Note
 
     # make a database if not exists
